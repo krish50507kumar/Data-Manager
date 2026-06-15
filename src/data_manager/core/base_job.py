4c683bb39c0244ebe3d2ee881e2b31496010ed6a
krish50507kumar <krish50507@gmail.com>
2026-06-16 01:37:49 +0530

    fixed the pipeline issue related to savepoint and roleback

--- src/data_manager/core/base_job.py
+++ src/data_manager/core/base_job.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 
 class BaseJob(ABC):
-    def __init__(self):
-        pass
+    def __init__(self,storage):
+        self.storage = storage
+
     @abstractmethod
     def run(self,context):
         pass
 
+    def savepoint(self,name):
+        self.storage.savepoint(name)
+    def roleback(self):
+        self.storage.roleback()
