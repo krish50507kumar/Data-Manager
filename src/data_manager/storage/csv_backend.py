664607d9b48719d1bbc5936f063c8194af1f7408
krish50507kumar <krish50507@gmail.com>
2026-06-15 19:58:01 +0530

    builded the folder structure of the feature

--- src/data_manager/storage/csv_backend.py
+++ src/data_manager/storage/csv_backend.py
@@ -1,6 +1,7 @@
 from data_manager.storage.base import BaseStorage
 import pandas as pd
 from pathlib import Path
+from collections import deque
 class CSVStorage(BaseStorage):
     """
         Storage backend for handling CSV files.
@@ -10,6 +11,8 @@ class CSVStorage(BaseStorage):
         super().__init__()
         self.data = None
         self.path = None | str
+        self.stack = deque()
+
     def store(self, data):
         """
         Stores a pandas DataFrame in the instance.
